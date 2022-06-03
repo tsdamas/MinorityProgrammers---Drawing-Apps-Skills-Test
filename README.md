8728577010af378c4450d95eff787e42febfa480
Thais Damasceno <tssdamasceno@gmail.com>
2022-06-02 19:20:35 -0700

    Set up the structure

---
+++ README.md
@@ -0,0 +1,2 @@
+# drawing-app
+ Drawing application for people to express themselves. 
---
+++ drawing-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <h1 className="title">Let's get drawing</h1>
+      <div className="drawing-space">
+        <canvas />
+      </div>
+    </div>
+  );
+}
+
+export default App;
